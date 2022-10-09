08a6fc4ce37907c7fbd8646b4689827502ae134c
VarinCode <94356640+VarinCode@users.noreply.github.com>
2022-10-09 19:33:40 +0700

    Update footer.jsx

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -15,10 +15,10 @@ function Footer(){
                         <footer class="py-3 my-4">
                             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                                 <li class="nav-item"><a href="mailto:warinsaipanya471@protonmail.com" class="nav-link px-2 text-muted"><i class="ri-mail-unread-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://follow-my-ig.web.app/" class="nav-link px-2 text-muted"><i class="ri-instagram-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://add-friends-7eead.web.app/" class="nav-link px-2 text-muted"><i class="ri-line-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://add-friend-github.web.app/" class="nav-link px-2 text-muted"><i class="ri-github-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://join-group-discord.web.app/" class="nav-link px-2 text-muted"><i class="ri-discord-fill"></i></a></li>
+                                 <li className="nav-item"><a href="https://add-friend-instagram.netlify.app/" className="nav-link px-2 text-dark"><i className="ri-instagram-fill"></i></a></li>
+                                <li className="nav-item"><a href="https://add-friend-line.netlify.app/"   className="nav-link px-2 text-dark"><i className="ri-line-fill"></i></a></li>
+                                <li className="nav-item"><a href="https://add-friend-github.netlify.app/"  className="nav-link px-2 text-dark"><i className="ri-github-fill"></i></a></li>
+                                <li className="nav-item"><a href="https://discord-invite-029wedw.netlify.app/"  className="nav-link px-2 text-dark"><i className="ri-discord-fill"></i></a></li>
                             </ul>
                             <p class="text-center text-dark">&copy; 2022 All Rights Reserved.</p>
                         </footer>
